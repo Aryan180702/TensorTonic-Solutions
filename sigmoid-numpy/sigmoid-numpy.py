a04c4264d01b3ee3485282e6ae52a1f9f5e513e3
Aryan180702 <86684471+Aryan180702@users.noreply.github.com>
2026-07-04 01:42:59 +0530

    Solve sigmoid-numpy

---
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Vectorized sigmoid function.
+    """
+    # Write code here
+    x = np.asarray(x, dtype=np.float32)
+
+    return 1/(1+np.exp(-x))
+    
